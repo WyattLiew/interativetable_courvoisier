14d1c98e7850da6cd8bf14c014df9aadd9f06fa5
Wei Junn <weijunn.liew@steppl.com.sg>
2020-02-27 11:52:16 +0800

    changed flip game

--- js/flipgame.js
+++ js/flipgame.js
@@ -1,6 +1,6 @@
 // Flip game js
 // Declare varible
-var flip_array = ['0','0','0','0','1','0','2','0','0','0','0','0','0','0','2','0','0','0'];
+var flip_array = ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','2','0','0','0'];
 var flip_values = [];
 var flip_tile_ids = [];
 var tiles_flipped = 0;
@@ -39,50 +39,9 @@ function newBoard() {
 			output += '<div id="tile_'+i+'"class="flipgame__bottle" ontouchstart="flipTile(this,\''+flip_array[i]+'\')"></div>';
 		}
 		document.getElementById('flip_board').innerHTML = output;
-
-		 // randomNum = Math.floor(Math.random()*Math.floor(2)+1);
-	// 	 randomNum =2;
-	// 	 if (randomNum == 1) {
-	// 	for (var i =0 ; i < flip_array.length; i++) {
-	// 			document.getElementById(`tile_${i}`).style.background = `url(assets/games/flip/courvoisier-${2}.png) no-repeat`;
-	// 			document.getElementById(`tile_${i}`).style.backgroundSize = `contain`;
-	// 			document.getElementById(`tile_${i}`).style.backgroundPosition = `center`;
-	// 	}
-	// } else {
-	// 	for (var i =0 ; i < flip_array.length; i++) {
-	// 			document.getElementById(`tile_${i}`).style.background = `url(assets/games/flip/courvoisier-${1}.png) no-repeat`;
-	// 			document.getElementById(`tile_${i}`).style.backgroundSize = `contain`;
-	// 			document.getElementById(`tile_${i}`).style.backgroundPosition = `center`;
-	// 	}
-	// }
 			
 
 	}
-
-let ended = 0;
-function flipTile(tile,val) {
-	if (tile.innerHTML=="") {
-		if (val == '2') {
-			tile.style.background = "transparent";
-			tile.innerHTML = `<div class="flipgame__tile">drink up!</div><img class="flipgame__tileImg" src="assets/games/flip/beercup.png">`;
-			tiles_flipped +=1;
-		} else if (val == '1') {
-			tile.style.background = "transparent";
-			tile.innerHTML = `<div class="flipgame__tile2">drink up!</div><img class="flipgame__tileImg" src="assets/games/flip/beercup.png">`;
-			tiles_flipped +=1;
-		}
-		else{
-		tile.style.background = "transparent";
-		tile.innerHTML ="";
-		tiles_flipped +=1;
-	}
-	// setTimeout(checkIfFinished,1000);
-	if(tiles_flipped == 18 && ended == 0){
-	ended =1;
-	checkIfFinished();
-	}
-	}
-}
 
 function checkIfFinished() {
 		var timer = 500;
@@ -98,6 +57,26 @@ function checkIfFinished() {
 				ended = 0;
 			}
 		},500);
+}
+
+function flipTile(tile,val) {
+	if (tile.innerHTML=="") {
+		if (tiles_flipped==0) {
+		tile.style.transition = "1s";
+		tile.style.transform = "scale(0)";
+		if (val == '2') {
+			tile.innerHTML = `<img class="flipgame__tileImg2" src="assets/games/flip/beercup.png">`;
+			tile.style.transition = "3s";
+			tile.style.background = "transparent";
+			tile.style.transform = "";
+			tiles_flipped +=1;
+			checkIfFinished();
+			}
+		else{
+			
+	}	
+}
+	}
 }
 
 // newBoard();
